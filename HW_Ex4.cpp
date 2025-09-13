eb6223738f9f3ff7334d88d88c1e1ec36b1886f4
nehabhati516 <141733196+nehabhati516@users.noreply.github.com>
2025-09-13 23:34:22 +0530

    Code with text output

--- HW_Ex4.cpp
+++ HW_Ex4.cpp
@@ -2,25 +2,29 @@
 
 NinjaIoT iot;
 
-bool infra = LOW;
+
+bool infra = 0; //declaring status of infrared sensor.
+char status[18] = "Sensor  is  clear"; //[declaring status string which shows the status of the sensor.]
 
 void setup() {
   Serial.begin(115200);
   iot.connect("wifi", "pwd", "uid");   //link: https://iot.roboninja.in/
   pinMode(D3, INPUT);
-  pinMode(D5, OUTPUT);
 }
 
 void loop() {
-  iot.SyncIN("D3");
   infra = digitalRead(D3); // Read the analog value from A0 pin
-  iot.WriteVar("InfraredDetection", infra); // Send the value to the cloud
-  if (infra == HIGH) {
-    digitalWrite(D5, 1000);
+  if (infra == 1) {
+    strcpy(status, "Sensor  is  clear"); //updating status if sensor is clear
   }
   else {
-    digitalWrite(D5, LOW);
+    strcpy(status, "!!Sensor blocked!!"); //updating status if sensor is blocked.
   }
   Serial.println(infra);
-  delay(50); // Wait for 1.5 seconds before sending the next value
+  Serial.println(status); //outputting the array status.
+  iot.WriteVar("InfraredDetection", infra); // Send the value to the cloud
+
+  //iot.WriteVar("Status", status); //The status array wasn't uploading for some reason. So I just commented it out.
+  
+  delay(50); // Wait for 0.5 seconds before sending the next value
 }
