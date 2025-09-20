62abf6152e69d2b4901958d5c8ce9ab9c4ccae24
nehabhati516 <141733196+nehabhati516@users.noreply.github.com>
2025-09-20 23:22:06 +0530

    hw7 finished!

--- 7class_hw/arduinocode_LED&IR.cpp
+++ 7class_hw/arduinocode_LED&IR.cpp
@@ -4,29 +4,30 @@ NinjaIoT iot;
 
 void setup() {
   Serial.begin(115200);
-  iot.connect("wifi", "pwd", "uid");   //link: https://iot.roboninja.in/
+  iot.connect(wifi, pwd, uid);   //link: https://iot.roboninja.in/
 }
 
 int manual = 0;
 // 0 = automatic
+// 1 = manual
 void loop() {
-  iot.SyncIN("D2");
+  iot.SyncIN("D2"); //sending IR reading to cloud
   delay(50);  // wait 50 milliseconds
-  iot.SyncOut("D6"); //connecting to D6.
+  iot.SyncOut("D6"); //reading the D6 value. D6 is connected to manual (will be updated later)
+  //D6 is connected to the dropdown for manual and automatic.
   iot.ReadAll(); // to update manual
 
-  manual = digitalRead(D6);
+  manual = digitalRead(D6); //linking manual with D6 reading.
 
-  if (manual == 0) {
-    if (digitalRead(D2) == LOW) {
-      digitalWrite(D5, HIGH);
+  if (manual == 0) { //if the dashboard is in automatic then
+    if (digitalRead(D2) == LOW) { //if IR sensor detects something then
+      digitalWrite(D5, HIGH); //D5 port connects to LED. This lights up the LED.
     }
     else {
-      digitalWrite(D5, LOW);
+      digitalWrite(D5, LOW); //this dims the LED if IR sensor detects nothing.
     }
-    iot.SyncIN("D5");
   }
-  else {
-    iot.ReadAll();
+  else { //if in manual mode
+    iot.SyncOut("D5"); //Take reading from cloud about the value of D5 which comes from the toggle button of on or off.
   }
 }
